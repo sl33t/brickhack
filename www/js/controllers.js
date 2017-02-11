654307aee76cbe5e2ed9bb22603f7035cd2256c5
Josh Bicking <jhb2345@rit.edu>
2017-02-11 15:50:22 -0500

    add user/pass auth example code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,21 @@
 angular.module('starter.controllers', [])
 
-.controller('LoginCtr', function($scope) {})
+// Login controller
+.controller('LoginCtr', function($scope, LoginService, $ionicPopup, $state) {
+  $scope.data = {};
+
+  $scope.login = function() {
+    // TODO Facebook login magic
+    // This example just checks LoginService in service.js, provided a username and password.
+    LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+      $state.go('tab.dash');
+    }).error(function(data) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Login failed!',
+        template: 'Please check your credentials!'
+        });
+      });
+    }
+})
 
 .controller('DashboardCtr', function($scope) {});
--- www/js/services.js
+++ www/js/services.js
@@ -1,1 +1,28 @@
-angular.module('starter.services', []);
+angular.module('starter.services', [])
+
+// Verify login data
+.service('LoginService', function($q) {
+  return {
+    loginUser: function(name, pw) {
+      var deferred = $q.defer();
+      var promise = deferred.promise;
+
+      /// Small example case of user and secret
+      if (name == 'user' && pw == 'secret') {
+        deferred.resolve('Welcome ' + name + '!');
+      } else {
+        deferred.reject('Wrong credentials.');
+      }
+      
+      // Resolve promise if credentials are corrent, otherwise reject promise.
+      promise.success = function(fn) {
+        promise.then(fn);
+        return promise;
+      }
+      promise.error = function(fn) {
+        promise.then(null, fn);
+        return promise;
+      }
+      return promise;
+    }
+}});
