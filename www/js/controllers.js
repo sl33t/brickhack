38b92496d54e2db9c831c94879356914ffa9ec2b
rana <vemireddy.rana@gmail.com>
2017-02-12 02:59:19 -0500

    database now stores email

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,14 +8,14 @@ angular.module('starter.controllers', [])
       var provider = new firebase.auth.FacebookAuthProvider();
       firebase.auth().signInWithPopup(provider).then(function (authData) {
         console.log(authData);
-		var face_data = [authData.user.uid, authData.user.displayName];
+		var face_data = [authData.user.uid, authData.user.displayName, authData.user.email];
 		firebase.database().ref('users/' + face_data[0]).once('value').then(function(snapshot){
 			if(snapshot.val() !== null){
 				console.log('User exists');
 			}
 			else{
 				console.log('User doesn\'t exist');
-				firebase.database().ref('users/' + face_data[0]).set({'name' : face_data[1], 'str' : 0, 'dex' : 0, 'con' : 0, 'int' : 0, 'wis' : 0, 'cha' : 0});
+				firebase.database().ref('users/' + face_data[0]).set({'name' : face_data[1], 'email' : face_data[2],  'str' : 0, 'dex' : 0, 'con' : 0, 'int' : 0, 'wis' : 0, 'cha' : 0});
 			}
 			$state.go('app.profile');
 		});
