c7b0338f34402b7f2c6687bbb145d92e6330ce6d
Josh Bicking <jhb2345@rit.edu>
2017-02-11 21:59:15 -0500

    Native android FB login (through inAppBrowser)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,13 +17,31 @@ angular.module('starter.controllers', [])
       });
     }
   $scope.FBLogin = function() {
-      var provider = new firebase.auth.FacebookAuthProvider();
-      firebase.auth().signInWithPopup(provider).then(function (authData) {
-        console.log(authData);
-        $state.go('tab.profile');
-      }).catch(function(error) {
-        console.log(error);
-      });
+     facebookConnectPlugin.login(['email']).then( (response) => {
+         const facebookCredential = firebase.auth.FacebookAuthProvider
+             .credential(response.authResponse.accessToken);
+
+         firebase.auth().signInWithCredential(facebookCredential)
+         .then((success) => {
+             console.log("Firebase success: " + JSON.stringify(success));
+             this.userProfile = success;
+         })
+         .catch((error) => {
+             console.log("Firebase failure: " + JSON.stringify(error));
+         });
+
+     }).catch((error) => { console.log(error) });
+      $state.go('tab.profile');
+}
+  $scope.LoginContinue = function() {
+    firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      $state.go('tab.profile');
+    } else {
+      alert("You are not logged in!");
+    }
+    });
   }
 })
 
@@ -38,5 +56,32 @@ angular.module('starter.controllers', [])
 })
 
 .controller('ProfileCtr', function($scope) {})
-.controller('QuestCtr', function($scope) {})
+
+.controller('QuestCtr', function($scope) {
+  $scope.cameraInit = function() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+    function onDeviceReady() {
+        console.log(navigator.device.capture);
+    }
+  }
+
+  // capture callback
+  $scope.startVideo = function () {
+    var captureSuccess = function(mediaFiles) {
+      var i, path, len;
+      for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+        path = mediaFiles[i].fullPath;
+        // TODO send file to other person
+      }
+    };
+    // capture error callback
+    var captureError = function(error) {
+      navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
+    };
+
+    // start video capture
+    navigator.device.capture.captureVideo(captureSuccess, captureError, {limit:2});
+  }
+})
+
 .controller('FriendsCtr', function($scope) {});
